2cc1e9b6df9c7634847a03a22e2077ad42a84744
Leon <leondo123@gmail.com>
2017-05-18 01:30:46 -0400

    queries

--- mongo.js
+++ mongo.js
@@ -3,13 +3,23 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/myDb');
 
 //make model
-var myModel = mongoose.model("mycollections", {price:Number})
+var myModel = mongoose.model("mycollections", {price:Number, email:Array, sent: Boolean})
 
 //create and save data
-myModel({price: 22}).save()
+myModel({price: 99, sent:false}).save()
 
 
-// query
-myModel.find({}, function (err, data) {
-        console.log(data)
+//find
+myModel.find({$and: [{ price: { $gte: 21 }}, {sent:false}]}, function (err, data) {
+    console.log(data)
+})
+
+// //update sent to true
+myModel.update({$and: [{ price: { $gte: 21 }}, {sent:false}]}, {$set:{sent:true}}, {multi: true}, function(err, data){
+	console.log(data)
+})
+
+// //add email to array
+myModel.update({ price: 22 }, {$push: {email: "email11@gmail.com"}}, function(err, data){
+	console.log(data)
 })
